LMODroid/platform_packages_apps_Updater

updater: Redesign preferences dialog
Co-authored-by: IacobIonut01 <paulionut2003@gmail.com>
Change-Id: Ice1bc5ac5069e8954621ab6a6b27c757c53c4dc2

src/com/libremobileos/updater/UpdatesActivity.java
@@ -54,6 +54,7 @@ import com.libremobileos.updater.misc.Constants;
 import com.libremobileos.updater.misc.StringGenerator;
 import com.libremobileos.updater.misc.Utils;
 import com.libremobileos.updater.model.UpdateInfo;
+import com.libremobileos.updater.ui.PreferenceSheet;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.snackbar.Snackbar;
 
@@ -326,80 +327,6 @@ public class UpdatesActivity extends UpdatesListActivity {
 
     @SuppressLint("ClickableViewAccessibility")
     private void showPreferencesDialog() {
-        View view = LayoutInflater.from(this).inflate(R.layout.preferences_dialog, null);
-        Spinner autoCheckInterval = view.findViewById(R.id.preferences_auto_updates_check_interval);
-        Switch autoDelete = view.findViewById(R.id.preferences_auto_delete_updates);
-        Switch dataWarning = view.findViewById(R.id.preferences_mobile_data_warning);
-        Switch abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
-        Switch updateRecovery = view.findViewById(R.id.preferences_update_recovery);
-
-        if (!Utils.isABDevice()) {
-            abPerfMode.setVisibility(View.GONE);
-        }
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        autoCheckInterval.setSelection(Utils.getUpdateCheckSetting(this));
-        autoDelete.setChecked(prefs.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false));
-        dataWarning.setChecked(prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true));
-        abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
-
-        if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
-            // Hide the update feature if explicitly requested.
-            // Might be the case of A-only devices using prebuilt vendor images.
-            updateRecovery.setVisibility(View.GONE);
-        } else if (Utils.isRecoveryUpdateExecPresent()) {
-            updateRecovery.setChecked(
-                    SystemProperties.getBoolean(Constants.UPDATE_RECOVERY_PROPERTY, false));
-        } else {
-            // There is no recovery updater script in the device, so the feature is considered
-            // forcefully enabled, just to avoid users to be confused and complain that
-            // recovery gets overwritten. That's the case of A/B and recovery-in-boot devices.
-            updateRecovery.setChecked(true);
-            updateRecovery.setOnTouchListener(new View.OnTouchListener() {
-                private Toast forcedUpdateToast = null;
-
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (forcedUpdateToast != null) {
-                        forcedUpdateToast.cancel();
-                    }
-                    forcedUpdateToast = Toast.makeText(getApplicationContext(),
-                            getString(R.string.toast_forced_update_recovery), Toast.LENGTH_SHORT);
-                    forcedUpdateToast.show();
-                    return true;
-                }
-            });
-        }
-
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.menu_preferences)
-                .setView(view)
-                .setOnDismissListener(dialogInterface -> {
-                    prefs.edit()
-                            .putInt(Constants.PREF_AUTO_UPDATES_CHECK_INTERVAL,
-                                    autoCheckInterval.getSelectedItemPosition())
-                            .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, autoDelete.isChecked())
-                            .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, dataWarning.isChecked())
-                            .putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
-                            .apply();
-
-                    if (Utils.isUpdateCheckEnabled(this)) {
-                        UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(this);
-                    } else {
-                        UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(this);
-                        UpdatesCheckReceiver.cancelUpdatesCheck(this);
-                    }
-
-                    if (Utils.isABDevice()) {
-                        boolean enableABPerfMode = abPerfMode.isChecked();
-                        mUpdaterService.getUpdaterController().setPerformanceMode(enableABPerfMode);
-                    }
-                    if (Utils.isRecoveryUpdateExecPresent()) {
-                        boolean enableRecoveryUpdate = updateRecovery.isChecked();
-                        SystemProperties.set(Constants.UPDATE_RECOVERY_PROPERTY,
-                                String.valueOf(enableRecoveryUpdate));
-                    }
-                })
-                .show();
+        new PreferenceSheet().setupPreferenceSheet(mUpdaterService).show(getSupportFragmentManager(), "prefdialog");
     }
 }

src/com/libremobileos/updater/ui/PreferenceSheet.kt
@@ -1,0 +1,134 @@
+package com.libremobileos.updater.ui
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.SystemProperties
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.Toast
+import androidx.preference.PreferenceManager
+import com.libremobileos.updater.R
+import com.libremobileos.updater.UpdatesCheckReceiver
+import com.libremobileos.updater.controller.UpdaterService
+import com.libremobileos.updater.misc.Constants
+import com.libremobileos.updater.misc.Utils
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+@SuppressLint("UseSwitchCompatOrMaterialCode")
+class PreferenceSheet : BottomSheetDialogFragment() {
+
+    private var prefs: SharedPreferences? = null
+
+    private var mUpdaterService: UpdaterService? = null
+    
+    private lateinit var preferencesAbPerfMode: Switch
+    private lateinit var preferencesAutoDeleteUpdates: Switch
+    private lateinit var preferencesMobileDataWarning: Switch
+    private lateinit var preferencesUpdateRecovery: Switch
+    private lateinit var preferencesAutoUpdatesCheckInterval: Spinner
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.preferences_dialog, container, false)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(view) {
+            preferencesAbPerfMode = findViewById(R.id.preferences_ab_perf_mode)
+            preferencesAutoDeleteUpdates = findViewById(R.id.preferences_auto_delete_updates)
+            preferencesMobileDataWarning = findViewById(R.id.preferences_mobile_data_warning)
+            preferencesUpdateRecovery = findViewById(R.id.preferences_update_recovery)
+            preferencesAutoUpdatesCheckInterval = findViewById(R.id.preferences_auto_updates_check_interval)
+        }
+        if (!Utils.isABDevice())
+            preferencesAbPerfMode.visibility = View.GONE
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        preferencesAutoUpdatesCheckInterval.setSelection(Utils.getUpdateCheckSetting(requireContext()))
+        preferencesAutoDeleteUpdates.isChecked =
+            prefs!!.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false)
+        preferencesMobileDataWarning.isChecked =
+            prefs!!.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true)
+        preferencesAbPerfMode.isChecked =
+            prefs!!.getBoolean(Constants.PREF_AB_PERF_MODE, false)
+
+        if (resources.getBoolean(R.bool.config_hideRecoveryUpdate)) {
+            // Hide the update feature if explicitly requested.
+            // Might be the case of A-only devices using prebuilt vendor images.
+            preferencesUpdateRecovery.visibility = View.GONE
+        } else if (Utils.isRecoveryUpdateExecPresent()) {
+            preferencesUpdateRecovery.isChecked =
+                SystemProperties.getBoolean(Constants.UPDATE_RECOVERY_PROPERTY, false)
+        } else {
+            // There is no recovery updater script in the device, so the feature is considered
+            // forcefully enabled, just to avoid users to be confused and complain that
+            // recovery gets overwritten. That's the case of A/B and recovery-in-boot devices.
+            preferencesUpdateRecovery.isChecked = true
+            preferencesUpdateRecovery.setOnTouchListener(object : View.OnTouchListener {
+                private var forcedUpdateToast: Toast? = null
+                override fun onTouch(v: View, event: MotionEvent): Boolean {
+                    if (forcedUpdateToast != null) {
+                        forcedUpdateToast!!.cancel()
+                    }
+                    forcedUpdateToast = Toast.makeText(
+                        requireContext(),
+                        getString(R.string.toast_forced_update_recovery), Toast.LENGTH_SHORT
+                    )
+                    forcedUpdateToast?.show()
+                    return true
+                }
+            })
+        }
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        prefs!!.edit()
+            .putInt(
+                Constants.PREF_AUTO_UPDATES_CHECK_INTERVAL,
+                preferencesAutoUpdatesCheckInterval.selectedItemPosition
+            )
+            .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, preferencesAutoDeleteUpdates.isChecked)
+            .putBoolean(Constants.PREF_MOBILE_DATA_WARNING, preferencesMobileDataWarning.isChecked)
+            .putBoolean(Constants.PREF_AB_PERF_MODE, preferencesAbPerfMode.isChecked)
+            .apply()
+
+        if (Utils.isUpdateCheckEnabled(requireContext())) {
+            UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(requireContext())
+        } else {
+            UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(requireContext())
+            UpdatesCheckReceiver.cancelUpdatesCheck(requireContext())
+        }
+
+        if (Utils.isABDevice()) {
+            val enableABPerfMode: Boolean = preferencesAbPerfMode.isChecked
+            mUpdaterService?.updaterController?.setPerformanceMode(enableABPerfMode)
+        }
+        if (Utils.isRecoveryUpdateExecPresent()) {
+            val enableRecoveryUpdate: Boolean = preferencesUpdateRecovery.isChecked
+            SystemProperties.set(
+                Constants.UPDATE_RECOVERY_PROPERTY,
+                enableRecoveryUpdate.toString()
+            )
+        }
+        super.onDismiss(dialog)
+    }
+
+    fun setupPreferenceSheet(updaterService: UpdaterService): PreferenceSheet {
+        this.mUpdaterService = updaterService
+        return this
+    }
+
+}

KanishkTheDerp/updater

Signed-off-by: Alvin Francis <nivlafx@gmail.com>
Signed-off-by: Kanishk <kanishkthederp@gmail.com>

app/src/main/java/com/voltage/updater/UpdatesActivity.java
@@ -67,6 +67,7 @@ import com.voltage.updater.misc.Constants;
 import com.voltage.updater.misc.StringGenerator;
 import com.voltage.updater.misc.Utils;
 import com.voltage.updater.model.UpdateInfo;
+import com.voltage.updater.ui.PreferenceSheet;
 
 import java.io.File;
 import java.io.IOException;
@@ -370,83 +371,6 @@ public class UpdatesActivity extends UpdatesListActivity {
 
     @SuppressLint("ClickableViewAccessibility")
     private void showPreferencesDialog() {
-        View view = LayoutInflater.from(this).inflate(R.layout.preferences_dialog, null);
-        Spinner autoCheckInterval = view.findViewById(R.id.preferences_auto_updates_check_interval);
-        Switch autoDelete = view.findViewById(R.id.preferences_auto_delete_updates);
-        Switch meteredNetworkWarning = view.findViewById(
-                R.id.preferences_metered_network_warning);
-        Switch abPerfMode = view.findViewById(R.id.preferences_ab_perf_mode);
-        Switch updateRecovery = view.findViewById(R.id.preferences_update_recovery);
-
-        if (!Utils.isABDevice()) {
-            abPerfMode.setVisibility(View.GONE);
-        }
-
-        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(this);
-        autoCheckInterval.setSelection(Utils.getUpdateCheckSetting(this));
-        autoDelete.setChecked(prefs.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false));
-        meteredNetworkWarning.setChecked(prefs.getBoolean(Constants.PREF_METERED_NETWORK_WARNING,
-                prefs.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true)));
-        abPerfMode.setChecked(prefs.getBoolean(Constants.PREF_AB_PERF_MODE, false));
-
-        if (getResources().getBoolean(R.bool.config_hideRecoveryUpdate)) {
-            // Hide the update feature if explicitly requested.
-            // Might be the case of A-only devices using prebuilt vendor images.
-            updateRecovery.setVisibility(View.GONE);
-        } else if (Utils.isRecoveryUpdateExecPresent()) {
-            updateRecovery.setChecked(
-                    SystemProperties.getBoolean(Constants.UPDATE_RECOVERY_PROPERTY, false));
-        } else {
-            // There is no recovery updater script in the device, so the feature is considered
-            // forcefully enabled, just to avoid users to be confused and complain that
-            // recovery gets overwritten. That's the case of A/B and recovery-in-boot devices.
-            updateRecovery.setChecked(true);
-            updateRecovery.setOnTouchListener(new View.OnTouchListener() {
-                private Toast forcedUpdateToast = null;
-
-                @Override
-                public boolean onTouch(View v, MotionEvent event) {
-                    if (forcedUpdateToast != null) {
-                        forcedUpdateToast.cancel();
-                    }
-                    forcedUpdateToast = Toast.makeText(getApplicationContext(),
-                            getString(R.string.toast_forced_update_recovery), Toast.LENGTH_SHORT);
-                    forcedUpdateToast.show();
-                    return true;
-                }
-            });
-        }
-
-        new AlertDialog.Builder(this)
-                .setTitle(R.string.menu_preferences)
-                .setView(view)
-                .setOnDismissListener(dialogInterface -> {
-                    prefs.edit()
-                            .putInt(Constants.PREF_AUTO_UPDATES_CHECK_INTERVAL,
-                                    autoCheckInterval.getSelectedItemPosition())
-                            .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, autoDelete.isChecked())
-                            .putBoolean(Constants.PREF_METERED_NETWORK_WARNING,
-                                    meteredNetworkWarning.isChecked())
-                            .putBoolean(Constants.PREF_AB_PERF_MODE, abPerfMode.isChecked())
-                            .apply();
-
-                    if (Utils.isUpdateCheckEnabled(this)) {
-                        UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(this);
-                    } else {
-                        UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(this);
-                        UpdatesCheckReceiver.cancelUpdatesCheck(this);
-                    }
-
-                    if (Utils.isABDevice()) {
-                        boolean enableABPerfMode = abPerfMode.isChecked();
-                        mUpdaterService.getUpdaterController().setPerformanceMode(enableABPerfMode);
-                    }
-                    if (Utils.isRecoveryUpdateExecPresent()) {
-                        boolean enableRecoveryUpdate = updateRecovery.isChecked();
-                        SystemProperties.set(Constants.UPDATE_RECOVERY_PROPERTY,
-                                String.valueOf(enableRecoveryUpdate));
-                    }
-                })
-                .show();
+        new PreferenceSheet().setupPreferenceSheet(mUpdaterService).show(getSupportFragmentManager(), "prefdialog");
     }
 }

app/src/main/java/com/voltage/updater/ui/PreferenceSheet.kt
@@ -1,0 +1,135 @@
+package com.rising.updater.ui
+
+import android.annotation.SuppressLint
+import android.content.DialogInterface
+import android.content.SharedPreferences
+import android.os.Bundle
+import android.os.SystemProperties
+import android.view.LayoutInflater
+import android.view.MotionEvent
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Spinner
+import android.widget.Switch
+import android.widget.Toast
+import androidx.preference.PreferenceManager
+import com.rising.updater.R
+import com.rising.updater.UpdatesCheckReceiver
+import com.rising.updater.controller.UpdaterService
+import com.rising.updater.misc.Constants
+import com.rising.updater.misc.Utils
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+
+@SuppressLint("UseSwitchCompatOrMaterialCode")
+class PreferenceSheet : BottomSheetDialogFragment() {
+
+    private var prefs: SharedPreferences? = null
+
+    private var mUpdaterService: UpdaterService? = null
+    
+    private lateinit var preferencesAbPerfMode: Switch
+    private lateinit var preferencesAutoDeleteUpdates: Switch
+    private lateinit var preferencesMeteredNetworkWarning: Switch
+    private lateinit var preferencesUpdateRecovery: Switch
+    private lateinit var preferencesAutoUpdatesCheckInterval: Spinner
+
+    override fun getTheme(): Int = R.style.BottomSheetDialogTheme
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return inflater.inflate(R.layout.preferences_dialog, container, false)
+    }
+
+    @SuppressLint("ClickableViewAccessibility")
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        with(view) {
+            preferencesAbPerfMode = findViewById(R.id.preferences_ab_perf_mode)
+            preferencesAutoDeleteUpdates = findViewById(R.id.preferences_auto_delete_updates)
+            preferencesMeteredNetworkWarning = findViewById(R.id.preferences_metered_network_warning)
+            preferencesUpdateRecovery = findViewById(R.id.preferences_update_recovery)
+            preferencesAutoUpdatesCheckInterval = findViewById(R.id.preferences_auto_updates_check_interval)
+        }
+        if (!Utils.isABDevice())
+            preferencesAbPerfMode.visibility = View.GONE
+
+        prefs = PreferenceManager.getDefaultSharedPreferences(requireContext())
+        preferencesAutoUpdatesCheckInterval.setSelection(Utils.getUpdateCheckSetting(requireContext()))
+        preferencesAutoDeleteUpdates.isChecked =
+            prefs!!.getBoolean(Constants.PREF_AUTO_DELETE_UPDATES, false)
+        preferencesMeteredNetworkWarning.isChecked =
+            prefs!!.getBoolean(Constants.PREF_METERED_NETWORK_WARNING,
+                prefs!!.getBoolean(Constants.PREF_MOBILE_DATA_WARNING, true))
+        preferencesAbPerfMode.isChecked =
+            prefs!!.getBoolean(Constants.PREF_AB_PERF_MODE, false)
+
+        if (resources.getBoolean(R.bool.config_hideRecoveryUpdate)) {
+            // Hide the update feature if explicitly requested.
+            // Might be the case of A-only devices using prebuilt vendor images.
+            preferencesUpdateRecovery.visibility = View.GONE
+        } else if (Utils.isRecoveryUpdateExecPresent()) {
+            preferencesUpdateRecovery.isChecked =
+                SystemProperties.getBoolean(Constants.UPDATE_RECOVERY_PROPERTY, false)
+        } else {
+            // There is no recovery updater script in the device, so the feature is considered
+            // forcefully enabled, just to avoid users to be confused and complain that
+            // recovery gets overwritten. That's the case of A/B and recovery-in-boot devices.
+            preferencesUpdateRecovery.isChecked = true
+            preferencesUpdateRecovery.setOnTouchListener(object : View.OnTouchListener {
+                private var forcedUpdateToast: Toast? = null
+                override fun onTouch(v: View, event: MotionEvent): Boolean {
+                    if (forcedUpdateToast != null) {
+                        forcedUpdateToast!!.cancel()
+                    }
+                    forcedUpdateToast = Toast.makeText(
+                        requireContext(),
+                        getString(R.string.toast_forced_update_recovery), Toast.LENGTH_SHORT
+                    )
+                    forcedUpdateToast?.show()
+                    return true
+                }
+            })
+        }
+    }
+
+    override fun onDismiss(dialog: DialogInterface) {
+        prefs!!.edit()
+            .putInt(
+                Constants.PREF_AUTO_UPDATES_CHECK_INTERVAL,
+                preferencesAutoUpdatesCheckInterval.selectedItemPosition
+            )
+            .putBoolean(Constants.PREF_AUTO_DELETE_UPDATES, preferencesAutoDeleteUpdates.isChecked)
+            .putBoolean(Constants.PREF_METERED_NETWORK_WARNING, preferencesMeteredNetworkWarning.isChecked)
+            .putBoolean(Constants.PREF_AB_PERF_MODE, preferencesAbPerfMode.isChecked)
+            .apply()
+
+        if (Utils.isUpdateCheckEnabled(requireContext())) {
+            UpdatesCheckReceiver.scheduleRepeatingUpdatesCheck(requireContext())
+        } else {
+            UpdatesCheckReceiver.cancelRepeatingUpdatesCheck(requireContext())
+            UpdatesCheckReceiver.cancelUpdatesCheck(requireContext())
+        }
+
+        if (Utils.isABDevice()) {
+            val enableABPerfMode: Boolean = preferencesAbPerfMode.isChecked
+            mUpdaterService?.updaterController?.setPerformanceMode(enableABPerfMode)
+        }
+        if (Utils.isRecoveryUpdateExecPresent()) {
+            val enableRecoveryUpdate: Boolean = preferencesUpdateRecovery.isChecked
+            SystemProperties.set(
+                Constants.UPDATE_RECOVERY_PROPERTY,
+                enableRecoveryUpdate.toString()
+            )
+        }
+        super.onDismiss(dialog)
+    }
+
+    fun setupPreferenceSheet(updaterService: UpdaterService): PreferenceSheet {
+        this.mUpdaterService = updaterService
+        return this
+    }
+
+}